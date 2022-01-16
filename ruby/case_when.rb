aba4b467e27a3577b1bf4a43a08eae8109ccafca
Ryota Higashi <ryo19870130@outlook.jp>
2022-01-16 23:17:57 +0900

    case when修正

---
+++ ruby/case_when.rb
@@ -0,0 +1,14 @@
+color = "black", "white"
+
+case color
+  when "red"
+    print "赤"
+  when "blue"
+    print "青"
+  when "black", "white"
+    print "パンダの色"
+  else
+    print ("その他")
+end
+
+# 出力：パンダの色
