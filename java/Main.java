764641e464dcaf9bb4febbcf202cd15aa4845c96
Ryota Higashi <ryo19870130@outlook.jp>
2022-01-31 23:36:45 +0900

    java

---
+++ java/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int num = 2;
+
+        switch(num){
+        case 1:
+            System.out.println("Aクラス");
+            break;
+        case 2:
+            System.out.println("Bクラス");
+            break;
+        case 3:
+            System.out.println("Cクラス");
+            break;
+        }
+    }
+}
